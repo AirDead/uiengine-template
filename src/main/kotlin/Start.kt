99621afaa6110fa3f9d2f6a87c42673b334a4d22
Air <160431936+AirDead@users.noreply.github.com>
2024-06-17 19:09:27 +0200

    Add files via upload

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -1,26 +1,31 @@
 package ru.airdead.mirageapi
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.server.command.CommandManager.literal
+import net.minecraft.text.Text
 import ru.airdead.uiengine.UIEngine
 import ru.airdead.uiengine.UIManager
-import ru.airdead.uiengine.utility.BLACK
-import ru.airdead.uiengine.utility.CENTER
-import ru.airdead.uiengine.utility.V3
-import ru.airdead.uiengine.utility.rectangle
+import ru.airdead.uiengine.utility.menu
 
 
 class Start : ClientModInitializer {
     override fun onInitializeClient() {
         UIEngine.initialize()
-        
-        val rectangle = rectangle { 
-            align = CENTER
-            origin = CENTER
-            color = BLACK
-            size = V3(200.0, 100.0)
+
+        val rectangle = menu {
         }
-        
+
         UIManager.addElement(rectangle)
+
+        CommandRegistrationCallback.EVENT.register { dispatcher, registryAccess, environment ->
+            dispatcher.register(literal("foo")
+                .executes { context ->
+                    rectangle.show()
+                    context.getSource().sendFeedback({ Text.literal("Called /foo with no arguments") }, false)
+                    1
+                })
+        }
     }
 
 }
