2ae11cde82f103051ea05b8230984faa6dfe8633
Air <160431936+AirDead@users.noreply.github.com>
2024-06-17 19:43:07 +0300

    Update Start.kt

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -1,3 +1,26 @@
-fun main() {
-    UIEn
+package ru.airdead.mirageapi
+
+import net.fabricmc.api.ClientModInitializer
+import ru.airdead.uiengine.UIEngine
+import ru.airdead.uiengine.UIManager
+import ru.airdead.uiengine.utility.BLACK
+import ru.airdead.uiengine.utility.CENTER
+import ru.airdead.uiengine.utility.V3
+import ru.airdead.uiengine.utility.rectangle
+
+
+class Start : ClientModInitializer {
+    override fun onInitializeClient() {
+        UIEngine.initialize()
+        
+        val rectangle = rectangle { 
+            align = CENTER
+            origin = CENTER
+            color = BLACK
+            size = V3(200.0, 100.0)
+        }
+        
+        UIManager.addElement(rectangle)
+    }
+
 }
