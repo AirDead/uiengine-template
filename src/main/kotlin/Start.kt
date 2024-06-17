8e967a42e49e32d08ac64946aa6e4ae0a9864407
Air <160431936+AirDead@users.noreply.github.com>
2024-06-17 19:16:32 +0200

    Add files via upload

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -6,21 +6,32 @@ import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.text.Text
 import ru.airdead.uiengine.UIEngine
 import ru.airdead.uiengine.UIManager
-import ru.airdead.uiengine.utility.menu
+import ru.airdead.uiengine.utility.*
 
 
 class Start : ClientModInitializer {
     override fun onInitializeClient() {
         UIEngine.initialize()
 
-        val rectangle = menu {
-        }
-
-        UIManager.addElement(rectangle)
 
         CommandRegistrationCallback.EVENT.register { dispatcher, registryAccess, environment ->
             dispatcher.register(literal("foo")
                 .executes { context ->
+                    val rectangle = menu {
+                        +rectangle {
+                            size = V3(200.0, 100.0)
+                            color = BLACK
+                            origin = CENTER
+                            align = CENTER
+                            +text {
+                                color = WHITE
+                                content = "Hello"
+                                autoFit = false
+                            }
+                        }
+                    }
+
+                    UIManager.addElement(rectangle)
                     rectangle.show()
                     context.getSource().sendFeedback({ Text.literal("Called /foo with no arguments") }, false)
                     1
